388a988cc1a3315c8da8cd074b4a37dd5df0defe
DenjiMk2 <13076835+DenjiMk2@users.noreply.github.com>
2018-09-22 23:44:09 +0900

    fast-commit

---
+++ spec/copy-line-number-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CopyLineNumberView from '../lib/copy-line-number-view';
+
+describe('CopyLineNumberView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
